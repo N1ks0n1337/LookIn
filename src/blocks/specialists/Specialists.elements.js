9afd05e28ae1cca6ab76752c1629e29b5e130e5a
Nick <sirvach666@gmail.com>
2022-05-11 01:44:34 +0500

    add specialists

--- src/blocks/specialists/Specialists.elements.js
+++ src/blocks/specialists/Specialists.elements.js
@@ -1,9 +1,6 @@
 import styled from "styled-components";
-import FirstBg from '../../image/specialists/FirstBg.png'
-import SecondBg from '../../image/specialists/SecondBg.png'
-import ThirdBg from '../../image/specialists/ThBg.png'
-import FourthBg from '../../image/specialists/FourthBg.png'
-import FifthBg from '../../image/specialists/FifthBg.png'
+
+import ManBg from '../../image/specialists/ManBg.png'
 import WomanBg from '../../image/specialists/WomanBg.png'
 
 export const Container = styled.div`
@@ -18,42 +15,124 @@ export const Wrapper = styled.div`
     height: 100%;
     margin: auto;
 `
-export const BlocksWrapper = styled.div`
+export const SpecialistsWrapper = styled.div`
     display: flex;
-    position: relative;
     justify-content: center;
-    margin-top: 60px;
-    @media(max-width: 916px){
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-}
+    align-content: center;
+    flex-direction: column;
 `
-export const BlockWrapperOne = styled.div`
-    position: absolute;
-    background-image: ${SecondBg};
-    top: 0;
-    left: 10px;
+export const Block = styled.div`
+    display: flex;
+    @media (min-width: 750px){
+        justify-content: space-evenly;
+        margin-bottom: -270px;
+        :nth-child(2n){
+        flex-direction: row-reverse;
+        }
+    }
+    @media (max-width: 750px) {
+        flex-direction: column-reverse;
+        margin: 60px auto;
+    }
+    @media (max-width: 750px) {
+        :not(:first-child){
+            margin-top: 480px;
+        }
+    }
+    @media (max-width: 540px) {
+        :not(:first-child){
+            margin-top: 350px;
+        }
+    }
+    @media (max-width: 390px) {
+        :not(:first-child){
+            margin-top: 250px;
+        }
+    }
+
 `
-export const BlockWrapperTwo = styled.div`
-    position: absolute;
-    top: 0;
-    left: 20px;
+export const ImageContainer = styled.div`
+    position: relative;
+
 `
-export const BlockWrapperThree = styled.div`
-    position: absolute;
-    top: 0;
-    left: 30px;
+export const Image = styled.img`
+    width: 774px;
+    @media (max-width: 1074px) {
+        width: 550px;
+    }
+    @media (max-width: 750px) {
+        position: absolute;
+        top: -80px;
+        left: -180px;
+    }
+    @media (max-width: 540px) {
+        width: 400px;
+        top: -50px;
+        left: -107px;
+    }
+    @media (max-width: 390px) {
+        width: 300px;
+        top: -50px;
+        left: -60px;
+    }
 `
-export const BlockWrapperFour = styled.div`
-    position: absolute;
-    top: 0;
-    left: 40px;
+export const TextContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+`
+
+export const TextNumber = styled.h2`
+    display: flex;
+    margin: 0 auto;
+    background: linear-gradient(to top, #fff, #3499ea);
+    background-size: 100%;
+    -webkit-background-clip: text;
+    -moz-background-clip: text;
+    -webkit-text-fill-color: transparent; 
+    -moz-text-fill-color: transparent;
+    font-size: 74px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.14;
+    letter-spacing: normal;
+    text-align: left;
+    @media (max-width: 1074px) {
+        font-size: 50px;
+    }
+`
+export const Text = styled.h3`
+    font-size: 32px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.25;
+    letter-spacing: normal;
+    text-align: center;
+    color: #2e2e2e;
+    @media (max-width: 1074px) {
+        font-size: 20px;
+    }
+`
+export const EnterImg = styled.img`
+    width: 22px;
+    margin-right: 19px;
+    margin-top: 10px;
+    @media (max-width: 1074px) {
+        width: 15px;
+        margin-top: 7px;
+    }
 `
 export const Line = styled.hr`
     border-bottom: 1px solid #2e2e2e;
     width: 30px;
     margin: 17px 13px;
+`
+export const BlueLine = styled.hr`
+    border-bottom: 1px solid #9ecef5;
+    width: 30px;
+    margin: 30px 13px;
 `
 export const Title = styled.div`
     margin-top: 20px;
@@ -63,17 +142,43 @@ export const Title = styled.div`
     color: #2e2e2e;
     font-size: 50px;
     padding-top: 50px;
-    padding-bottom: 81px;
+    padding-bottom: 15px;
     @media(max-width: 798px){
         font-size: 37px
     }
-    @media(max-width: 611px){
+    @media(max-width: 708px){
         font-size: 30px
     }
-    @media(max-width: 561px){
+    @media(max-width: 596px){
         font-size: 20px
     }
+    @media(max-width: 432px){
+        font-size: 15px
+    }
+    @media(max-width: 355px){
+        font-size: 12px
+    }
     
+`
+export const SubTitle = styled.div`
+    font-size: 22px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.82;
+    letter-spacing: normal;
+    text-align: center;
+    color: #2e2e2e;
+    margin-bottom: -100px;
+    @media(max-width: 611px){
+        font-size: 20px
+    }
+    @media(max-width: 596px){
+        font-size: 16px
+    }
+    @media(max-width: 432px){
+        font-size: 12px
+    }
 `
 export const TitleBlue = styled.span`
     color: #3499ea;
--- src/blocks/specialists/Specialists.js
+++ src/blocks/specialists/Specialists.js
@@ -1,37 +1,71 @@
 import React from 'react';
-
+import FirstBg from '../../image/specialists/FirstBg.png'
+import SecondBg from '../../image/specialists/SecondBg.png'
+import ThirdBg from '../../image/specialists/ThirdBg.png'
+import FourthBg from '../../image/specialists/FourthBg.png'
+import Enter from '../../image/specialists/Enter.svg'
 import {
     Container,
     Wrapper,
     Line,
     Title,
     TitleBlue,
-    BlocksWrapper,
-    BlockWrapperOne,
-    BlockWrapperTwo,
-    BlockWrapperThree,
-    BlockWrapperFour
+    SpecialistsWrapper,
+    Block,
+    ImageContainer,
+    Image,
+    TextContainer,
+    TextNumber,
+    Text,
+    EnterImg,
+    SubTitle,
+    BlueLine,
 } from './Specialists.elements';
 
 const Specialists = props => {
     return (
         <Container>
             <Wrapper>
-            <Title><Line/>Вы можете выбрать&nbsp;<TitleBlue>специалиста</TitleBlue><Line/></Title>
-            <BlocksWrapper>
-                <BlockWrapperOne>
-                    
-                </BlockWrapperOne>
-                <BlockWrapperTwo>
-                    f
-                </BlockWrapperTwo>
-                <BlockWrapperThree>
-                    g
-                </BlockWrapperThree>
-                <BlockWrapperFour>
-                    h
-                </BlockWrapperFour>
-            </BlocksWrapper>
+                <Title><Line/>Вы можете выбрать&ensp;<TitleBlue>специалиста</TitleBlue><Line/></Title>
+                <SubTitle>с которым вам будет комфортно</SubTitle>
+                <SpecialistsWrapper>
+                    <Block>
+                        <ImageContainer>
+                            <Image src={FirstBg}/>
+                        </ImageContainer>
+                        <TextContainer>
+                            <TextNumber><BlueLine/>01<BlueLine/></TextNumber>
+                            <Text><EnterImg src={Enter}/>Заполняете<br/>анкету</Text>
+                        </TextContainer>
+                    </Block>
+                    <Block>
+                        <ImageContainer>
+                            <Image src={FirstBg}/>
+                        </ImageContainer>
+                        <TextContainer>
+                            <TextNumber><BlueLine/>02<BlueLine/></TextNumber>
+                            <Text><EnterImg src={Enter}/>Регестрируйтесь<br/> на сайте</Text>
+                        </TextContainer>
+                    </Block>
+                    <Block>
+                        <ImageContainer>
+                            <Image src={FirstBg}/>
+                        </ImageContainer>
+                        <TextContainer>
+                            <TextNumber><BlueLine/>03<BlueLine/></TextNumber>
+                            <Text><EnterImg src={Enter}/>Регестрируйтесь<br/> на сайте</Text>
+                        </TextContainer>
+                    </Block>
+                    <Block>
+                        <ImageContainer>
+                            <Image src={FirstBg}/>
+                        </ImageContainer>
+                        <TextContainer>
+                            <TextNumber><BlueLine/>04<BlueLine/></TextNumber>
+                            <Text><EnterImg src={Enter}/>Регестрируйтесь<br/> на сайте</Text>
+                        </TextContainer>
+                    </Block>
+                </SpecialistsWrapper>
             </Wrapper>
         </Container>
     );
